b451244c89976010f8c766a1b472f698891a4629
hoisitgoing <howisitgoing@kakao.com>
2022-09-13 19:30:39 +0900

    소프트웨어 아키텍처

---
+++ src/main/java/kr/ac/uos/designpattern/DesignpatternApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.uos.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternApplication.class, args);
+    }
+
+}
